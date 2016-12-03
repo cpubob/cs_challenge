e487866d5daad8ae6f2c2d5ae9c85e15aa96a5c3
Robert Laidlaw <cpubob@cpubob-macbookpro.roam.corp.google.com>
2016-12-03 16:11:10 -0500

    Clear solution on refresh

--- app.py
+++ app.py
@@ -3,15 +3,19 @@ from flask import Flask, render_template,request
 app = Flask(__name__)
 vars = {}
 
+
 @app.route('/')
 def get_root():
     vars['solution'] = None
+    vars['response'] = None
+
     return render_template("index.html", vars=vars)
 
 
 @app.route('/', methods=['POST'])
 def post_data():
     vars['solution'] = request.form['Solution']
+    vars['response'] = 'Success'
     return render_template("index.html", vars=vars)
 
 if __name__ == "__main__":
